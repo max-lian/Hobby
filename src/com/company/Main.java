6251d3eb8c87d80f7faba62bf66622165b14a26f
max-lian <max-lian@users.noreply.github.com>
2019-10-17 12:35:59 +0300

    Add in main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
+import com.company.enums.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Hobby[] hobby = new Hobby[2];
+        hobby[0] = new Football("Football", Complexity.HARD, 200,"big", WorkFoot.RIGHT );
+        hobby[1] = new Fishing("Fishing", Complexity.NORMAL, 100, 15.2f,2.5f );
+        for (Hobby hobb:hobby) {
+            System.out.println(hobb.toString());
+        }
     }
 }
